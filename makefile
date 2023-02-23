ec9f55c19b30991f0fe3de492f25bd8d6bda5cfc
archmans <13521010@std.stei.itb.ac.id>
2023-02-23 17:40:30 +0700

    Add : protected mode ( belum fix )

--- makefile
+++ makefile
@@ -30,7 +30,8 @@ kernel:
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/kernel.c -o $(OUTPUT_FOLDER)/kernel.o
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/portio.c -o $(OUTPUT_FOLDER)/portio.o
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/stdmem.c -o $(OUTPUT_FOLDER)/stdmem.o
-	@$(LIN) $(LFLAGS) $(OUTPUT_FOLDER)/kernel_loader.o $(OUTPUT_FOLDER)/kernel.o $(OUTPUT_FOLDER)/portio.o $(OUTPUT_FOLDER)/stdmem.o -o $(OUTPUT_FOLDER)/kernel
+	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/gdt.c -o $(OUTPUT_FOLDER)/gdt.o
+	@$(LIN) $(LFLAGS) $(OUTPUT_FOLDER)/kernel_loader.o $(OUTPUT_FOLDER)/kernel.o $(OUTPUT_FOLDER)/portio.o $(OUTPUT_FOLDER)/stdmem.o $(OUTPUT_FOLDER)/gdt.o -o $(OUTPUT_FOLDER)/kernel
 	@echo Linking object files and generate elf32...
 	@rm -f *.o
 
@@ -39,5 +40,5 @@ iso: kernel
 	@cp $(OUTPUT_FOLDER)/kernel     $(OUTPUT_FOLDER)/iso/boot/
 	@cp other/grub1                 $(OUTPUT_FOLDER)/iso/boot/grub/
 	@cp $(SOURCE_FOLDER)/menu.lst   $(OUTPUT_FOLDER)/iso/boot/grub/
-	genisoimage -R -b boot/grub/grub1 -no-emul-boot -boot-load-size 4 -A os -input-charset utf8 -quiet -boot-info-table -o OS2023.iso bin/iso
+	@genisoimage -R -b boot/grub/grub1 -no-emul-boot -boot-load-size 4 -A os -input-charset utf8 -quiet -boot-info-table -o OS2023.iso bin/iso
 	@rm -r $(OUTPUT_FOLDER)/iso/
---
+++ src/gdt.c
@@ -0,0 +1,65 @@
+#include "lib-header/stdtype.h"
+#include "lib-header/gdt.h"
+
+/**
+ * global_descriptor_table, predefined GDT.
+ * Initial SegmentDescriptor already set properly according to GDT definition in Intel Manual & OSDev.
+ * Table entry : [{Null Descriptor}, {Kernel Code}, {Kernel Data (variable, etc)}, ...].
+ */
+struct GlobalDescriptorTable global_descriptor_table = {
+  .table = {
+    {
+      .segment_low = 0,
+      .base_low = 0,
+      .base_mid = 0,
+      .base_high = 0,
+      .type_bit = 0,
+      .non_system = 0,
+      .descriptor_privilege_level = 0,
+      .present = 0,
+      .segment_high = 0,
+      .available = 0,
+      .code_segment = 0,
+      .default_operation_size = 0,
+      .granularity = 0
+    },
+    {
+      .segment_low = 0xFFFF,
+      .base_low = 0,
+      .base_mid = 0,
+      .type_bit = 0xA,
+      .non_system = 1,
+      .descriptor_privilege_level = 0,
+      .present = 1,
+      .segment_high = 0xF,
+      .available = 0,
+      .code_segment = 1,
+      .default_operation_size = 1,
+      .granularity = 1,
+    },
+    {
+      .segment_low = 0xFFFF,
+      .base_low = 0,
+      .base_mid = 0,
+      .type_bit = 0x2,
+      .non_system = 1,
+      .descriptor_privilege_level = 0,
+      .present = 1,
+      .segment_high = 0xF,
+      .available = 0,
+      .code_segment = 0,
+      .default_operation_size = 1,
+      .granularity = 1,
+    },
+  }
+};
+
+/**
+ * _gdt_gdtr, predefined system GDTR. 
+ * GDT pointed by this variable is already set to point global_descriptor_table above.
+ * From: https://wiki.osdev.org/Global_Descriptor_Table, GDTR.size is GDT size minus 1.
+ */
+struct GDTR _gdt_gdtr = {
+    .size = sizeof(struct GlobalDescriptorTable) - 1,
+    .address = &global_descriptor_table
+};

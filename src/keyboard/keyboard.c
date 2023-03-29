9a7f5d54678902f48e39c065b8399b2032d9dadb
malikbaharsyah <13521029@mahasiswa.itb.ac.id>
2023-03-29 09:00:54 +0700

    handle backspace

--- src/keyboard/keyboard.c
+++ src/keyboard/keyboard.c
@@ -4,6 +4,7 @@
 #include "../lib-header/stdmem.h"
 
 static struct KeyboardDriverState keyboard_state;
+static uint8_t cursor_x=0, cursor_y=0;
 
 const char keyboard_scancode_1_to_ascii_map[256] = {
       0, 0x1B, '1', '2', '3', '4', '5', '6',  '7', '8', '9',  '0',  '-', '=', '\b', '\t',
@@ -42,23 +43,41 @@ bool is_keyboard_blocking(void){
     return keyboard_state.keyboard_input_on;
 }
 
-uint8_t cursor_x=0, cursor_y=0;
+void get_last_line_idx(uint8_t* curX, uint8_t* curY) {
+  bool flag = FALSE;
+  uint8_t i;
+  while(!flag) {
+    i=80;
+    (*curX)--;
+    while(MEMORY_FRAMEBUFFER[((*curX) * 80 + i) * 2] == 0 ) {
+    i--;
+    if (MEMORY_FRAMEBUFFER[((*curX) * 80 + i) * 2] != 0) {
+      flag = TRUE;
+      break;
+    }
+    }
+  }
+  (*curY) = i;
+}
+
 void keyboard_isr(void) {
     if (!keyboard_state.keyboard_input_on)
         keyboard_state.buffer_index = 0;
     else {
         uint8_t  scancode    = in(KEYBOARD_DATA_PORT);
         char     mapped_char = keyboard_scancode_1_to_ascii_map[scancode];
-
         // TODO : Implement scancode processing
         
-
-        // handle backspace
+          // handle backspace
         if (mapped_char == '\b') {
           if (cursor_y > 0) {
-
-            framebuffer_write(cursor_x, cursor_y-1, 0, 0, 0);
-            cursor_y--;
+            keyboard_state.buffer_index--;
+            framebuffer_write(cursor_x, cursor_y-1, 0, 0xF, 0);
+            cursor_y = cursor_y-1;
+          } else if (cursor_x > 0) {
+            keyboard_state.buffer_index = 0;
+            get_last_line_idx(&cursor_x, &cursor_y);
+            framebuffer_write(cursor_x, cursor_y, 0, 0xF, 0);
           }
         }
 
@@ -67,13 +86,16 @@ void keyboard_isr(void) {
             keyboard_state.keyboard_buffer[keyboard_state.buffer_index] = 0;
             keyboard_state.buffer_index = 0;
             keyboard_state.keyboard_input_on = FALSE;
-            framebuffer_write(cursor_x, cursor_y, 0, 0, 0);
+            framebuffer_write(cursor_x, cursor_y, ' ', 0xF, 0);
             cursor_y = 0;
             cursor_x++;
         }
 
+        //handle normal characters
         else if (mapped_char != 0) {
-          framebuffer_write(cursor_x, cursor_y, mapped_char, 0, 0xF);
+          keyboard_state.keyboard_buffer[keyboard_state.buffer_index] = mapped_char;
+          keyboard_state.buffer_index++;
+          framebuffer_write(cursor_x, cursor_y, mapped_char, 0xF, 0);
           cursor_y++;
           if (cursor_y == 80) {
             cursor_y = 0;
@@ -82,6 +104,7 @@ void keyboard_isr(void) {
         }
 
       framebuffer_set_cursor(cursor_x, cursor_y);
+      
     }
     pic_ack(IRQ_KEYBOARD);
 }
